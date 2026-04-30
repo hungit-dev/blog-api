8915e8b20343172a9887e3e0378b32ede752d9d5
hungit-dev <hungitdev@gmail.com>
2026-04-30 12:52:50 -0600

    fix (backend): allow it to read json from the body. The verified user req does not contain password

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 require("dotenv").config({ path: "../.env" });
-require('./passport-auth-config');  //register passport jwt strategy
+require("./passport-auth-config"); //register passport jwt strategy
 const express = require("express");
 const session = require("express-session");
 const path = require("node:path");
@@ -11,17 +11,20 @@ const app = express();
 
 /*read form submissions and use it values*/
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //initialize passport
-app.use(passport.initialize())
+app.use(passport.initialize());
+
 //use router
 app.use("/", blogApiRouter);
 
 //set the port
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
     console.error("Error starting the server:", error);
-    throw error; 
+    throw error;
   }
   console.log(`Server running on port ${PORT}`);
 });
--- backend/passport-auth-config.js
+++ backend/passport-auth-config.js
@@ -1,19 +1,24 @@
 const passport = require("passport");
-const {prisma}=require("./lib/prisma")
-const JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-const opts = {}
+const { prisma } = require("./lib/prisma");
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
 
-passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
-     try {
-      const user = await prisma.user.findUnique({ where: { id: jwt_payload.id } }); //jwt_payload will be passed to JwtStrategy after .authenticate() verify
-      if (!user) return done(null, false);   
-      return done(null, user);          
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: jwt_payload.id },
+        select: { id: true, email: true, username: true, role: true },
+      });
+      if (!user) return done(null, false);
+      return done(null, user);
     } catch (err) {
-      return done(err, false);            
+      return done(err, false);
     }
-}));
+  }),
+);
 
 module.exports = passport;
